dc312dc2cf375102366de200654b919e56ccf1be
Joffrey Porée <contact@joffreyporee.com>
2016-09-10 17:51:33 +0200

    Test package

---
+++ src/MongoDB/ODM/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Test
+ *
+ * @author poree
+ */
+class Test {
+    public function __construct() {
+        echo "test";
+    }
+}
