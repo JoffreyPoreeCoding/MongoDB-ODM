157d1053a34bf89241bf7bbca1e672c5bbf48ef7
Joffrey Porée <contact@joffreyporee.com>
2017-01-04 20:43:08 +0100

    Remove "MongoDB/ODM" dir in "src"

---
+++ src/Exception/AnnotationException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace JPC\MongoDB\ODM\Exception;
+
+/**
+ * Exception for annotation problems (missing, bad values, ...)
+ */
+class AnnotationException extends Exception {}
---
+++ src/Exception/Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace JPC\MongoDB\ODM\Exception;
+
+/**
+ * Define a base for JPC MongoDB ODM Exceptions
+ */
+class Exception extends \Exception {}
