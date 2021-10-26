d0aaa3d9af2de3eb4817d0cfde69d892dd029d70
Joffrey <poree.joffrey@gmail.com>
2021-10-26 21:26:57 +0000

    :alembic: Test codecov

--- tests/MyTest.php
+++ tests/MyTest.php
@@ -1,11 +1,14 @@
 <?php
 
+require __DIR__ . '/../src/MyFile.php';
+
 use PHPUnit\Framework\TestCase;
 
 class MyTest extends TestCase
 {
     public function testTest()
     {
-        $this->assertTrue(true);
+        $c = new Test();
+        $this->assertTrue($c->oui());
     }
 }
