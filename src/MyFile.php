44159a53be05d7fda66c04828a0e9e8edb1e311b
Joffrey <poree.joffrey@gmail.com>
2021-10-26 21:22:49 +0000

    :building_construction: Update test configurations

---
+++ src/MyFile.php
@@ -0,0 +1,9 @@
+<?php
+
+class Test
+{
+    public function oui()
+    {
+        return true;
+    }
+}
---
+++ tests/MyTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
